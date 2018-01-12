dad7a777d5fba05fe63ff15ded6d05ef77d7cf1a
tigertian25 <34469688+tigertian25@users.noreply.github.com>
2018-01-12 17:06:16 +0800

    前台页面有问题

--- modules/web/src/com/artwellhk/alertsystem/web/alert/AlertBrowse.java
+++ modules/web/src/com/artwellhk/alertsystem/web/alert/AlertBrowse.java
@@ -1,6 +1,25 @@
 package com.artwellhk.alertsystem.web.alert;
 
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import com.artwellhk.alertsystem.entity.Alert;
+import com.artwellhk.alertsystem.web.myDatasource.AlertListDatasource;
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.data.DsBuilder;
+import com.haulmont.cuba.gui.data.GroupDatasource;
+import com.haulmont.cuba.gui.data.impl.CustomGroupDatasource;
 
 public class AlertBrowse extends AbstractLookup {
+	
+	@Override
+	public void init(Map<String, Object> params) {
+		/*GroupDatasource ds = new DsBuilder(getDsContext())
+		        .setJavaClass(Alert.class)
+		        .setDsClass(AlertListDatasource.class)
+		        .setId("alertsDs")
+		        .buildGroupDatasource();*/
+		new AlertListDatasource();
+	}
 }
